18b6d6304c1a33ec17dbe776e77adecbfcf8e6ac
vaux <zero.griffin@gmail.com>
2021-01-19 18:42:12 +0100

    Update scene

--- macOS/Game.swift
+++ macOS/Game.swift
@@ -1,8 +1,11 @@
 import SpriteKit
 
 final class Game: SKView, SKViewDelegate {
+    private let area: Area
+    
     required init?(coder: NSCoder) { nil }
     init(area: Area) {
+        self.area = area
         super.init(frame: .zero)
         translatesAutoresizingMaskIntoConstraints = false
         ignoresSiblingOrder = true
@@ -20,16 +23,38 @@ final class Game: SKView, SKViewDelegate {
         scene.addChild(square1)
         scene.addChild(square2)
     }
-    
-    func view(_: SKView, shouldRenderAtTime: TimeInterval) -> Bool {
-//        state.render(shouldRenderAtTime)
-        return true
-    }
 
     override func mouseDown(with: NSEvent) {
-        guard let scene = self.scene else { return }
-        scene.nodes(at: with.location(in: scene)).compactMap { $0 as? Square }.first.map {
-            $0.open()
+        scene!.nodes(at: with.location(in: scene!)).compactMap { $0 as? Square }.first.map {
+            $0.run(.fadeOut(withDuration: 0.5))
+            emitter($0.position)
         }
+    }
+    
+    private func emitter(_ position: CGPoint) {
+        let emitter = SKEmitterNode()
+        emitter.particleTexture = .init(imageNamed: "bubble")
+        emitter.particleSize = .init(width: 50, height: 50)
+        emitter.particleBirthRate = 30
+        emitter.emissionAngleRange = .pi * 2
+        emitter.particleRotationRange = .pi * 2
+        emitter.particleColor = SKColor(named: area.foreground)!
+        emitter.particleColorBlendFactor = 1
+        emitter.particleSpeed = 100
+        emitter.particleLifetime = 0.5
+        emitter.numParticlesToEmit = 9
+        emitter.particleAlphaSpeed = -0.6
+        emitter.particleRotationSpeed = 0.5
+        emitter.particlePosition = .zero
+        emitter.particlePositionRange = .zero
+        emitter.particleScaleRange = 0.2
+        emitter.particleScaleSpeed = 1.1
+        emitter.particleScale = 0.2
+        emitter.zPosition = -1
+        emitter.position = position
+        
+        scene!.addChild(emitter)
+        
+        emitter.run(.sequence([.wait(forDuration: 7), .removeFromParent()]))
     }
 }
